3184c1cfd26808073c86033e3a3a21d4f620be04
Jiaping Zeng <jiaping@gisar.me>
2019-10-13 20:51:24 -0700

    small fix

--- assignment2/script.sh
+++ assignment2/script.sh
@@ -2,8 +2,8 @@
 
 counter=1
 while IFS= read -r line; do
-    substring=$(echo $line | cut -c1-4)
-    if [ "$substring" = "<td>" ]; then
+    substring=$(echo $line | cut -c1-3)
+    if [ "$substring" = "<td" ]; then
         if [ $(( $counter & 1 )) -eq 1 ]; then
             word=$(echo "$line" | sed -e 's/<[^>]*>//g' | tr "\`" "\'" | tr -d '?.' | tr -s " " "\n" | tr -d "\-\n" | tr '[:upper:]' '[:lower:]')
             if [ ! -z "$word" ]; then
