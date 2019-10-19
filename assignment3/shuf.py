c239a95e6aea11236cbad209224cccbfd0f7eeff
Jiaping Zeng <jiaping@gisar.me>
2019-10-18 18:10:11 -0700

    finished shuf

--- assignment3/shuf.py
+++ assignment3/shuf.py
@@ -1,29 +1,55 @@
 #! /usr/bin/env python3
 
-import random, sys
-import argparse
+import random, sys, argparse
 
-def shuf(text):
-    lines = text.readlines()
-    random.shuffle(lines)
-    return lines
+class shuf:
+    def __init__(self, lines):
+        self.lines = lines
+
+    def shuf(self, head_count=""):
+        random.shuffle(self.lines)
+        for i in range(len(self.lines)):
+            print(self.lines[i])
+
+    def repeat_shuf(self, head_count=""):
+        if head_count:
+            for _ in range(int(head_count)):
+                print(random.choice(self.lines))
+        else:
+            while True:
+                print(random.choice(self.lines))
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--echo", action="store_true", help="treat each ARG as an input line")
     parser.add_argument("-n", "--head-count", action="store", help="output at most COUNT lines")
     parser.add_argument("-r", "--repeat", action="store_true", help="output lines can be repeated")
-    parser.add_argument("infile", nargs="?", type=argparse.FileType("r"), default=sys.stdin)
+    parser.add_argument("input", nargs="*")
     args = parser.parse_args()
 
-    lines = shuf(args.infile)
-    print(len(lines))
-    
-    count = len(lines)
-    if (args.head-count and count > head-count):
-        count = head-count
-    for i in range(count):
-        print(lines[i].rstrip('\n'))
+    lines = []
+    if args.input or args.echo:
+        if args.echo:
+            # treat each arg as an input line
+            lines = args.input
+        elif len(args.input) == 1:
+            # open file
+            with open(args.input[0]) as infile:
+                for line in infile.readlines():
+                    lines.append(line.rstrip('\n'))
+        else:
+            # input error
+            sys.exit("Too many arguments")
+    else:
+        # empty input, read stdin
+        for line in sys.stdin:
+            lines.append(line.rstrip('\n'))
+
+    s = shuf(lines)
+    if args.repeat:
+        s.repeat_shuf(args.head_count)
+    else:
+        s.shuf(args.head_count)
 
 if __name__ == "__main__":
     main()
