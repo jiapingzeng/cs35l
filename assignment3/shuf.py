eea70799d4c97c547051c4c5299e533049b8a411
Jiaping Zeng <jiaping@gisar.me>
2019-10-18 23:12:53 -0700

    inplemented input range

--- assignment3/shuf.py
+++ assignment3/shuf.py
@@ -22,17 +22,26 @@ class shuf:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--echo", action="store_true", help="treat each ARG as an input line")
+    parser.add_argument("-i", "--input-range", action="store", help="treat each number LO through HI as an input line")
     parser.add_argument("-n", "--head-count", action="store", help="output at most COUNT lines")
     parser.add_argument("-r", "--repeat", action="store_true", help="output lines can be repeated")
     parser.add_argument("input", nargs="*")
     args = parser.parse_args()
 
     lines = []
-    if args.input or args.echo:
-        if args.echo:
-            # treat each arg as an input line
-            lines = args.input
-        elif len(args.input) == 1:
+
+    if args.echo:
+        # treat each arg as an input line
+        lines = args.input
+    elif args.input_range:
+        # use input range
+        try:
+            split = args.input_range.split('-')
+            lines = list(range(int(split[0]), int(split[1]) + 1))
+        except:
+            sys.exit("Invalid input range")
+    elif args.input:
+        if len(args.input) == 1:
             # open file
             with open(args.input[0]) as infile:
                 for line in infile.readlines():
